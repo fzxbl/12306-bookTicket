267d7376a64e7c8a2c8859665b8d22c51b855f85
fzxbl <31810871+fzxbl@users.noreply.github.com>
2017-10-08 13:20:12 +0800

    购票脚本

---
+++ 购票.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+from bookticket import *
+obj=BookTicket()
+obj.submitOrderRequest()
+obj.confirmPassenger()
+obj.getPassenger()
+obj.checkOrderInfo()
+obj.confirmSingleForQueue()
+time.sleep(5)
